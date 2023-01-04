404db41e6b90cdc2e3664563b7a3cf393872f4d0
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-01-04 14:14:28 +0100

    added-and-blessed-delete-button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let dataCollection = document.querySelector('#dataForm');
+let victim = document.getElementById('')
 
 dataCollection.addEventListener("submit", function(event){
     event.preventDefault();
@@ -6,6 +7,7 @@ dataCollection.addEventListener("submit", function(event){
     let studentTable = document.getElementById('studentRecord');
     let newStudent = studentTable.insertRow(2);
     let nameCell = newStudent.insertCell(0);
+
     nameCell.textContent = studentFormData.get('inName');
     let sur1Cell = newStudent.insertCell(1);
     sur1Cell.textContent = studentFormData.get('inSur1');
@@ -15,5 +17,40 @@ dataCollection.addEventListener("submit", function(event){
     gradeCell.textContent = studentFormData.get('inGrade');
     let imgCell = newStudent.insertCell(4);
     imgCell.textContent = studentFormData.get('inFoto');
+    let graveYard = newStudent.insertCell(5);
+    graveYard.id = "angelOfDeath";
+    let gonnaDie = document.createElement('button');
+    gonnaDie.textContent = "ELIMINAR";
+    gonnaDie.className = 'waitForIt';
+    let gonnaKill = document.querySelector('#angelOfDeath');
+    gonnaKill.appendChild(gonnaDie);
+
+    gonnaDie.addEventListener("click", erase);
+
+    function erase(){
+        gonnaDie.closest('tr').remove();
+    }
     
 } )
+
+
+
+
+/*
+$('#table').on('click','.delete-row',function(){
+    $(this).closest('tr').remove();
+    // $(this) is the button
+    
+});
+let gonnaDie = document.querySelector('#waitForIt');
+gonnaDie.addEventListener("click", deleition());
+function deleition(){
+    gonnaDie.closest('tr').remove();
+}
+
+let gonnaDie = document.createElement('button');
+gonnaDie.addEventListener("click", deleition());
+function deleition(){
+gonnaDie.closest('tr').remove();
+}
+*/
